99bd702bf2611dd552f87457b669a4bdbb98a469
gumh <gumh>
2022-05-30 13:10:45 +0800

    tested on wireless, but no data readout

--- releases/env.sh
+++ releases/env.sh
@@ -4,5 +4,5 @@ export GRAND_DAQ_CONFIG_LOG=${GRAND_DAQ_CONFIG}/logging
 export LD_LIBRARY_PATH=${GRAND_DAQ_HOME}/external/lib:${GRAND_DAQ_HOME}/external/lib64:${GRAND_DAQ_HOME}/grand-daq/lib:${LD_LIBRARY_PATH}
 export PATH=${GRAND_DAQ_HOME}/grand-daq/bin:${PATH}
 
-export GRAND_DATA_DIR=/home/grand/workarea/data
+export GRAND_DATA_DIR=/home/wang/workarea/data
 export GRAND_MAX_FILE_SIZE=500000000 # 500MB per file
